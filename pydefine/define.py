c9175050392d90e1a3bb762e7cf98c3c8b07e610
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-07-26 13:10:45 +0800

    Use chat api

--- pydefine/define.py
+++ pydefine/define.py
@@ -1,13 +1,27 @@
 import json
 from backoff import on_exception, expo
-from pyllm import llm
+from pyopenaichat import chat, system, user
 
 
 @on_exception(expo, Exception, max_time=60)
 def define(word):
-    prompt = {
-        "task": "define",
-        "features": ["etymology", "grammar", "description"],
-        "word": word.lower(),
-    }
-    return llm(json.dumps(prompt))
+    messages = [
+        system(
+            json.dumps(
+                {
+                    "response": "plain text",
+                }
+            )
+        ),
+        user(
+            json.dumps(
+                {
+                    "define": ["etymology", "grammar", "description"],
+                    "word": word.lower(),
+                }
+            )
+        ),
+    ]
+    message = chat(messages)
+    content = message["content"]
+    return content
