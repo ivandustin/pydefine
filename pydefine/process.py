4467ea287c7ad506a950ed716a7824f374dc24dc
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2023-07-29 18:51:57 +0800

    Retry process

--- pydefine/process.py
+++ pydefine/process.py
@@ -1,9 +1,11 @@
+from backoff import on_exception, expo
 from .constants.extensions import TXT
 from .get_group import get_group
 from .define import define
 from .write import write
 
 
+@on_exception(expo, Exception)
 def process(directory, word):
     group = get_group(word)
     filename = word + TXT
